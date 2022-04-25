44d1ba150ec9f43b31e96cc34e8bdde1e67452ab
MANNE DEDEEPYA <dedeepyamanne02@gmail.com>
2022-04-25 07:51:49 +0530

    Entity and controller update

--- src/main/java/com/example/mongoBookStore/model/Book.java
+++ src/main/java/com/example/mongoBookStore/model/Book.java
@@ -6,34 +6,78 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.*;
 
 
-@Document(collection = "BookStore")
+@Document(collection = "Books")
 public class Book {
 
 	@Id
 	private int id;
-	private String bookName;
-	private String authorName;
+	private String author;
+    private String country;
+    private String imageLink;
+    private String language;
+    private String link;
+    private String pages;
+    private String title;
+    private String year;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getBookName() {
-		return bookName;
+	public String getAuthor() {
+		return author;
 	}
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
+	public void setAuthor(String author) {
+		this.author = author;
 	}
-	public String getAuthorName() {
-		return authorName;
+	public String getCountry() {
+		return country;
 	}
-	public void setAuthorName(String authorName) {
-		this.authorName = authorName;
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getImageLink() {
+		return imageLink;
+	}
+	public void setImageLink(String imageLink) {
+		this.imageLink = imageLink;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getPages() {
+		return pages;
+	}
+	public void setPages(String pages) {
+		this.pages = pages;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
 	}
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", bookName=" + bookName + ", authorName=" + authorName + "]";
+		return "Book [id=" + id + ", author=" + author + ", country=" + country + ", imageLink=" + imageLink
+				+ ", language=" + language + ", link=" + link + ", pages=" + pages + ", title=" + title + ", year="
+				+ year + "]";
 	}
 	
 	
--- src/main/java/com/example/mongoBookStore/resource/BookController.java
+++ src/main/java/com/example/mongoBookStore/resource/BookController.java
@@ -19,27 +19,53 @@ public class BookController {
 
 	@Autowired
 	private BookRepository bookRepository;
-	
+
 	@PostMapping("/addBook")
 	private String saveBook(@RequestBody Book book) {
+		long start = System.currentTimeMillis();
 		bookRepository.save(book);
+		long end = System.currentTimeMillis();
+		System.out.println("Save time for one record: " + (end-start));
 		return "Added book with id : "+book.getId();
 	}
-	
+
+	@PostMapping("/addBooks")
+	private String saveBooks(@RequestBody List<Book> books) {
+		long start = System.currentTimeMillis();
+		bookRepository.saveAll(books);
+		long end = System.currentTimeMillis();
+		System.out.println("Save time for all records : " + (end-start));
+		return "Added books";
+	}
+
 	@GetMapping("/findAllBooks")
 	public List getBooks() {
 		return bookRepository.findAll();
 	}
-	
-	@GetMapping("/findAllBooks/{id}")
+
+	@GetMapping("/findBook/{id}")
 	public Optional<Book> getBook(@PathVariable int id){
+		long start = System.currentTimeMillis();
 		bookRepository.findById(id);
+		long end = System.currentTimeMillis();
+		System.out.println("Fetch time for one record: " + (end-start));
 		return bookRepository.findById(id);
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
 	public String deleteBook(@PathVariable int id) {
-		 bookRepository.deleteById(id);
-		 return "Book Deleted with id : "+id;
+		long start = System.currentTimeMillis();
+		bookRepository.deleteById(id);
+		long end = System.currentTimeMillis();
+		System.out.println("Time to Delete one record : " + (end-start));
+		return "Book Deleted with id : "+id;
+	}
+	@DeleteMapping("/deleteAll")
+	public String deleteBooks() {
+		long start = System.currentTimeMillis();
+		bookRepository.deleteAll();
+		long end = System.currentTimeMillis();
+		System.out.println("Time to Delete all records : " + (end-start));
+		return "Deleted All Books";
 	}
 }
